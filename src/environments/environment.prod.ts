6bb552d21879af8f8792065a5b12aa1abcd51a01
enricsarra <enricsarra@gmail.com>
2020-06-12 14:11:23 +0200

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: 'b157d375adeb7860d2574fc3587e23e6',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: 'b157d375adeb7860d2574fc3587e23e6',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
 
 /*
